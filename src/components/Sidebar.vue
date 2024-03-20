f04db5959226daeb708598d154d3185ad27e25ff
Christian <christianpavilonis@outlook.com>
2024-03-20 13:12:41 -0500

    Makes sidebar look better

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="min-w-280">
-        <div class="bg-shade-5 w-280 h-[100vh] flex flex-col justify-between overflow-y-auto fixed z-1">
+        <div
+            class="sidebar bg-shade-5 w-280 h-[100vh] flex flex-col justify-between overflow-y-auto fixed z-1"
+        >
             <div>
                 <SidebarItem
                     v-for="conversation in conversationsStore.conversations"
@@ -21,7 +23,11 @@
                         stroke="currentColor"
                         class="w-24 h-24"
                     >
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15"/>
+                        <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M12 4.5v15m7.5-7.5h-15"
+                        />
                     </svg>
                 </button>
             </div>
@@ -30,16 +36,16 @@
 </template>
 
 <script lang="ts" setup>
-import {onMounted} from 'vue';
-import {useConversationsStore} from "../Lib/ConversationsStore";
-import {invoke} from "@tauri-apps/api";
-import {storeToRefs} from "pinia";
-import {createConversation} from "../Lib/helpers";
-import {useRoute, useRouter} from "vue-router";
+import { onMounted } from "vue";
+import { useConversationsStore } from "../Lib/ConversationsStore";
+import { invoke } from "@tauri-apps/api";
+import { storeToRefs } from "pinia";
+import { createConversation } from "../Lib/helpers";
+import { useRoute, useRouter } from "vue-router";
 import SidebarItem from "./SidebarItem.vue";
 
 let conversationsStore = useConversationsStore();
-let {conversations} = storeToRefs(conversationsStore);
+let { conversations } = storeToRefs(conversationsStore);
 let router = useRouter();
 let route = useRoute();
 
@@ -53,5 +59,7 @@ onMounted(async () => {
 </script>
 
 <style scoped>
-
+.sidebar::-webkit-scrollbar {
+    display: none;
+}
 </style>
--- src/components/SidebarItem.vue
+++ src/components/SidebarItem.vue
@@ -1,33 +1,22 @@
 <template>
     <RouterLink
         :to="`/conversation/${conversation.id}`"
-        class="px-20 py-16 flex hover:bg-shade-6 relative"
+        class="px-20 py-16 flex hover:bg-shade-6 relative text-sm"
         :class="isCurrentConversation(conversation.id) ? 'bg-shade-6' : ''"
     >
-        <svg
-            class="w-24 h-24 mr-12"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-        >
-            <path stroke-linecap="round" stroke-linejoin="round"
-                  d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 01.865-.501 48.172 48.172 0 003.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z"/>
-        </svg>
         {{ conversation.title || "New Chat" }}
     </RouterLink>
 </template>
 
 <script lang="ts" setup>
-import {ref} from 'vue';
-import {useConversationsStore} from "../Lib/ConversationsStore";
-import {storeToRefs} from "pinia";
-import {useRoute, useRouter} from "vue-router";
+import { ref } from "vue";
+import { useConversationsStore } from "../Lib/ConversationsStore";
+import { storeToRefs } from "pinia";
+import { useRoute, useRouter } from "vue-router";
 import ContextMenu from "./ContextMenu.vue";
 
 let conversationsStore = useConversationsStore();
-let {conversations} = storeToRefs(conversationsStore);
+let { conversations } = storeToRefs(conversationsStore);
 let router = useRouter();
 let route = useRoute();
 let rightClickEvent = ref();
@@ -36,10 +25,7 @@ function isCurrentConversation(id: string) {
     return route.params.id === id;
 }
 
-defineProps<{conversation: any}>();
-
+defineProps<{ conversation: any }>();
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
